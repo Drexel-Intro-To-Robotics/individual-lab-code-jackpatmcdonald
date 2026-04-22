8527d8e500aa7ccef2915c5659aed7e540d628a5
Jack McDonald <jpmcdonald3@gmail.com>
2026-04-22 19:24:50 +0000

    Initial commit

--- catkin_ws/src/lab1/scripts/my_turtlebot.py
+++ catkin_ws/src/lab1/scripts/my_turtlebot.py
@@ -1,20 +1,139 @@
 #!/usr/bin/env python3
 
+# ------------ Personal Notes ---------------
+'''
+-----How to run-----
+cd /workspaces/MEM571/catkin_ws
+catkin_make clean
+catkin_make
+source devel/setup.bash
+roslaunch lab1 controller.launch
 
 
-class myTurtle():
-    
-    
+-----RViz------
+Make sure fixed frame is set to odom
+'''
+#--------------------------------------------------
+
+import rospy
+import math
+from geometry_msgs.msg import Twist, Pose, PoseStamped
+from nav_msgs.msg import Odometry
+
+class myTurtle():    
     def __init__(self) -> None:
-        """_summary_
-        
-        create all the nessary pubs/subs here and all the nessary other things
-        
-        """
+        # Initialize the controller node
+        rospy.init_node('turtlebot3_controller', anonymous=True)
+        # Publisher Node
+        self.vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)
+        # Subscriber node - odometry
+        self.odom_sub = rospy.Subscriber('/odom', Odometry, self.odom_cb)
+
+        # Subscribe to Rviz
+        self.goal_sub = rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.goal_cb)
+
+        # Beginning Pose
+        self.current_x = 0.0
+        self.current_y = 0.0
+        self.current_theta = 0.0
+
+        # Publish Rate
+        self.rate = rospy.Rate(10)
+        # Wait for publisher/subscriber nodes
+        rospy.sleep(1.0)
         pass 
     
-        
+    # Drive turtlebot forward a given distance
+    def drive_straight(self, dist: float, vel: float)->None:
+        twist = Twist()
+        # Get current position (not pose)
+        start_x = self.current_x
+        start_y = self.current_y
 
+        # Set direction (forward (+) or backward (-))
+        if dist >= 0:
+            twist.linear.x = vel
+        else:
+            twist.linear.x = -vel
+        
+        # Drive until distance is reached
+        while not rospy.is_shutdown():
+            # Calculate distance
+            moved = math.sqrt((self.current_x - start_x)**2
+                              + (self.current_y - start_y)**2)
+            # Check if distance reached
+            if moved >= abs(dist):
+                break
+            self.vel_pub.publish(twist)
+            self.rate.sleep()
+        self.stop()
+        pass
+
+    # Rotate the robot
+    # Input angle in radians
+    def rotate(self, angle):
+        twist = Twist()
+        omega = 0.5
+        # Determine direction
+        if angle >= 0:
+            twist.angular.z = omega
+        else:
+            twist.angular.z = -omega
+
+        # Get current angle
+        start_theta = self.current_theta
+        total_angle = 0.0
+        previous_theta = start_theta
+
+        # Begin rotating
+        while not rospy.is_shutdown():
+            delta = self.current_theta - previous_theta
+            # Big maths - normalize delta
+            delta = (delta + math.pi) % (2*math.pi) - math.pi
+            # how much robot has turned
+            total_angle += abs(delta)
+            previous_theta = self.current_theta
+
+            if total_angle >= abs(angle):
+                break
+            self.vel_pub.publish(twist)
+            self.rate.sleep()
+        self.stop()
+        pass
+    
+    # Spin wheels
+    def spin_wheels(self, u1, u2, time):
+        # Turtlebot 3 parameters
+        wheel_base = 0.16
+        wheel_r = 0.033
+
+        # convert wheel speeds
+        linear_x = wheel_r*(u2+u1)/2
+        angular_z = wheel_r*(u2-u1)/wheel_base
+
+        twist = Twist()
+        twist.linear.x = linear_x
+        twist.angular.z = angular_z
+
+        # Get current time
+        start_time = rospy.Time.now()
+        while not rospy.is_shutdown():
+            time_passed = (rospy.Time.now() - start_time).to_sec()
+            if time_passed >= time:
+                break
+            self.vel_pub.publish(twist)
+            self.rate.sleep()
+        self.stop()
+        pass
+
+    # Stop the bot
+    def stop(self)->None:
+        # zero twist
+        self.vel_pub.publish(Twist())
+        rospy.sleep(0.5)
+        pass
+
+    # Navigate to pose
     def nav_to_pose(self, goal):
         # type: (PoseStamped) -> None
         """
@@ -23,71 +142,131 @@ class myTurtle():
         :param goal: PoseStamped
         :return:
         """
+        x_goal = goal.position.x
+        y_goal = goal.position.y
+
+        #convert to theta
+        q = goal.orientation
+        theta_goal = self.convert_to_euler(q.x, q.y, q.z, q.w)
+
+        # Rotate towards target coordinates
+        angle_to_target = math.atan2(y_goal - self.current_y,
+                                     x_goal - self.current_x)
+        # Get difference between position theta and current theta
+        angle_diff = angle_to_target - self.current_theta
+        # Take shortest turn to angle 
+        min_angle_diff = (angle_diff + math.pi) % (2*math.pi) - math.pi
+        # Go for it
+        self.rotate(min_angle_diff)
+
+        # Get the distance and drive that thang
+        distance = math.sqrt((x_goal-self.current_x)**2 + (y_goal-self.current_y)**2)
+        self.drive_straight(distance, 0.22)
+
+        # Get difference between current and desired theta
+        end_angle_diff = theta_goal - self.current_theta
+        # shortest turn
+        end_min_angle_diff = (end_angle_diff + math.pi) % (2*math.pi) - math.pi
+        # do the twist
+        self.rotate(end_min_angle_diff)
         pass
 
     def odom_cb(self,msg:Odometry) ->None:
-        """_summary_
+        # Get current pose
+        position = msg.pose.pose.position
+        orientation = msg.pose.pose.orientation
 
-        Get the odom and update the internal location of the robot
-        Args:
-            msg (Odometry): _description_
-        """
+        # Set pose
+        self.current_x = position.x
+        self.current_y = position.y
+
+        # Get orientation
+        self.current_theta = self.convert_to_euler(
+            orientation.x, orientation.y, 
+            orientation.z, orientation.w)
         pass
     
-    def stop(self)->None:
-        """_summary_
-        
-        Stop moving
-        """
-        pass
-        
-        
-    def drive_straight(self, dist: float, vel: float)->None:
-        """_summary_
+    # function for converting quaternation to angle
+    def convert_to_euler(self, x,y,z,w):
+        siny_cosp = 2.0*(w*z + x*y)
+        cosy_cosp = 1.0 - 2.0*(y*y + z*z)
+        return math.atan2(siny_cosp, cosy_cosp)    
 
-        Args:
-            dist (_type_): _description_
-        """
-        pass
-        
+    # Move the robot in a circle based on input radius
+    def drive_circle(self, radius: float) -> None:
+        speed = 0.2
+        omega = speed/radius # w = v/r
+
+        # get circumference
+        circumference = 2*math.pi*abs(radius)
+        duration = circumference/speed
+
+        twist = Twist()
+        twist.linear.x = speed
+        if radius > 0:
+            twist.angular.z = omega
+        else:
+            twist.angular.z = -omega
+
+        start_time = rospy.Time.now()
+        while not rospy.is_shutdown():
+            elapsed = (rospy.Time.now() - start_time).to_sec()
+            if elapsed >= duration:
+                break
+            self.vel_pub.publish(twist)
+            self.rate.sleep()  
     
-    def spin_wheels(self, u1, u2, time):
-        """
-        Spin the two wheels
-
-        :param u1: wheel 1 speed
-        :param u2: wheel 2 speed
-        :param time: time to drive
-        :return: None
-        """
-        pass
-
-    def rotate(self, angle):
-        """
-        Rotate in place
-        :param angle: angle to rotate
-        :return: None
-        """
-        pass
-    
-    def convert_to_euler(self, quat):
-        # type: (Quaternion) -> float
-        """
-        This might be helpful to have
-        :param quat: quaternion 
-        :return: euler angles
-        """
-        
+    # call when a 2d navgoal is set in rviz
+    def goal_cb(self,msg: PoseStamped) -> None:
+        rospy.loginfo(f"Goal pose created: x={msg.pose.position.x:.2f}, y={msg.pose.position.y:.2f}")
+        self.nav_to_pose(msg.pose)
 
 def main():
-    """_summary_
-    create all the node start up here
-    """
-    
-    
+    bot = myTurtle()
 
+    # Part 5 - Drive in a 0.5m circle
+    # bot.drive_circle(0.5)
+    # bot.stop()
 
+    # Part 6 - Drive in a 0.5m/side square
+    # bot.drive_straight(0.5, 0.15)
+    # rospy.sleep(0.25)
+    # bot.rotate(1.5)
+    # rospy.sleep(0.25)
+    # bot.drive_straight(0.5, 0.15)
+    # rospy.sleep(0.25)
+    # bot.rotate(1.5)
+    # rospy.sleep(0.25)
+    # bot.drive_straight(0.5, 0.15)
+    # rospy.sleep(0.25)
+    # bot.rotate(1.5)
+    # rospy.sleep(0.25)
+    # bot.drive_straight(0.5, 0.15)
+    # rospy.sleep(0.25)
+    # bot.rotate(1.5)
+
+    # Part 7 - (this is a bash - need to run from a new terminal)
+    # check the /scripts/ folder for the bash file
+
+    # Part 8 - Do a little dance
+    bot.rotate(1.5)
+    rospy.sleep(0.25)
+    bot.rotate(-1.5)
+    rospy.sleep(0.25)
+    bot.rotate(-1.5)
+    rospy.sleep(0.25)
+    bot.drive_straight(-0.5,0.2)
+    rospy.sleep(0.25)
+    bot.drive_straight(0.5,0.2)
+
+    def convert_to_euler(x,y,z,w):
+        siny_cosp = 2.0*(w*z + x*y)
+        cosy_cosp = 1.0 - 2.0*(y*y + z*z)
+        return math.atan2(siny_cosp, cosy_cosp)
+    
+    # Keep the node alive for rviz nav goals
+    # rospy.spin() 
 
 
 if __name__ == '__main__':
-    main()
+    main()   
--- searching_map_HW/read_map.py
+++ searching_map_HW/read_map.py
@@ -1,9 +1,20 @@
 import sys
 from PIL import Image
 import copy
-import Queue
+import queue
 import math
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
+
+'''
+---------------Personal Notes---------------------
+How to run)
+1. cd /workspaces/MEM571/searching_map_HW/maps/
+2. python3 /workspaces/MEM571/searching_map_HW/read_map.py trivial.gif
+-------------------------------------------------------
+'''
 
 '''
 These variables are determined at runtime and should not be changed or mutated by you
@@ -29,7 +40,7 @@ path = []  # an ordered list of (x,y) tuples, representing the path to traverse 
 expanded = {}  # a dictionary of (x,y) tuples, representing nodes that have been expanded
 frontier = {}  # a dictionary of (x,y) tuples, representing nodes to expand to in the future
 
-open = Queue.PriorityQueue()
+open = queue.PriorityQueue()
 came_from = {}
 cost_so_far = {}
 
@@ -39,9 +50,75 @@ def search(map):
     provided map.
     :param map: A '1-concept' PIL PixelAccess object to be searched. (basically a 2d boolean array)
     """
-    pass
-   
+    # Get image dimensions
+    im_dims = Image.open(difficulty)
+    width, height = im_dims.size
+    im_dims.close()
 
+    # Confirmation of start and end
+    print("Starting at : {}".format(start))
+    print("Goal : {}".format(end))
+
+    # Main Dijkstra loop
+    while not open.empty():
+        # go to node with lowest known cost
+        current_cost, current_node = open.get()
+
+        # check if we've gotten to this node at a cheaper cost
+        if current_node in expanded:
+            continue
+
+        # Mark the node as expanded (visited)
+        expanded[current_node] = current_cost
+
+        # Remove node from frontier since its been visited
+        if current_node in frontier:
+            del frontier[current_node]
+        
+        # Check if goal has been reached
+        if current_node == end:
+            node = end
+            while node is not None:
+                path.append(node)
+                node = came_from.get(node)
+            path.reverse()
+            print("The maze has been solved!")
+            print("Path Cost: {}".format(current_cost))
+            print("Length of path: {}".format(len(path)))
+            return 
+        
+        # Expand nearby steps
+        for step in next_steps(current_node, map, width, height):
+            # Set cost to 1
+            new_cost = cost_so_far[current_node] + 1
+
+            if step not in cost_so_far or new_cost < cost_so_far[step]:
+                cost_so_far[step] = new_cost
+                came_from[step] = current_node
+
+                # add to priority queue
+                open.put((new_cost, step))
+                # track in frontier
+                frontier[step] = new_cost
+
+    # sanity check if goal wasnt reached
+    print("No path found from {} to {}".format(start, end))
+    pass
+
+# Function for finding the next step options and their weights
+def next_steps(node, map, width, height):
+    x, y = node
+    steps = []
+
+    # Each direction) right, left, up, down
+    for dx, dy in [(1,0), (-1,0), (0,1), (0,-1)]:
+        nx, ny = x + dx, y + dy
+
+        # Check boundary
+        if 0 <= nx < width and 0 <= ny < height:
+            if map[nx,ny] != 0:
+                steps.append((nx,ny))
+    return steps
 
 
 def visualize_search(save_file="do_not_save.png"):
@@ -68,10 +145,10 @@ def visualize_search(save_file="do_not_save.png"):
         pixel_access[pixel[0], pixel[1]] = DARK_GRAY
 
     # display and (maybe) save results
-    im.show()
-    if (save_file != "do_not_save.png"):
-        im.save(save_file)
-
+    # im.show()
+    # if (save_file != "do_not_save.png"):
+    #     im.save(save_file)
+    im.save("solved_maze.png")
     im.close()
 
 if __name__ == "__main__":
@@ -83,7 +160,7 @@ if __name__ == "__main__":
     # Parse input arguments
     function_name = str(sys.argv[0])
     difficulty = str(sys.argv[1])
-    print("running " + function_name + " with " + difficulty + " difficulty.")
+    print("================ Running " + function_name + " with " + difficulty + " difficulty.==================")
 
     # Hard code start and end positions of search for each difficulty level
     if difficulty == "trivial.gif":
@@ -108,10 +185,11 @@ if __name__ == "__main__":
         assert False, "Incorrect difficulty level provided"
     G = 1000000000000000000
     E = 1000000000000000000
-    open.put((start, 0))
+    open.put((0, start))
     came_from[start] = None
     cost_so_far[start] = 0
     # Perform search on given image
     im = Image.open(difficulty)
     im = im.convert('1')
     search(im.load())
+    visualize_search("solved_maze.png")
